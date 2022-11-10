739a7bbde2bd9eefe91d3908a2be8e413d5f33c4
Soenke Sobott <sobott.soenke@gmail.com>
2022-11-10 12:15:04 +0100

    Initial Commit

---
+++ PeriSolutionFinder/PeriSolutionFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeriSolutionFinderApp.swift
+//  PeriSolutionFinder
+//
+//  Created by Sönke Sobott on 10.11.22.
+//
+
+import SwiftUI
+
+@main
+struct PeriSolutionFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

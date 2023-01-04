a4c71a6b9338012730238cd9d00b8131b6e6ab7b
Soenke Sobott <sobott.soenke@gmail.com>
2023-01-04 11:46:15 +0100

    PERI: Updated about page

--- PeriSolutionFinder/View/AboutView.swift
+++ PeriSolutionFinder/View/AboutView.swift
@@ -19,13 +19,21 @@ struct AboutView: View {
                     BackButton()
                 })
                 .padding(.leading, 20)
+                .frame(maxWidth: .infinity, alignment: .leading)
+
+                Text("About")
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .center)
+
                 Spacer()
+                    .frame(maxWidth: .infinity, alignment: .trailing)
             }
 
+
             VStack(alignment: .center, spacing: 10) {
-                Text("About")
                 Text("Used Libraries")
                 Text("[SDWebImageSwiftUI](https://github.com/SDWebImage/SDWebImageSwiftUI)")
+                Text("[RangeUISlider](https://github.com/chicio/RangeUISlider)")
             }
             .padding(.top, 20)
 
